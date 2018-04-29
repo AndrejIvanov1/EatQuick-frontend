258d0977e8c6b505f3349665325cfd57e9ea1190
Andrej Ivanov <ivandrej@gmail.com>
2018-04-29 10:03:44 +0100

    back from checker

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -83,7 +83,9 @@ const DisconnectedApp = ({
         }, 5000)
     }
 
-    const percentage = meals.breakfast.served.length / meals.breakfast.invited.length * 100
+    const breakfastPercentage = meals.breakfast.served.length / meals.breakfast.invited.length * 100
+    const lunchPercentage = meals.lunch.served.length / meals.lunch.invited.length * 100
+    const dinnerPercentage = meals.dinner.served.length / meals.dinner.invited.length * 100
 
     return (
       <div className="App">
@@ -98,36 +100,45 @@ const DisconnectedApp = ({
                 { !meals.breakfast.active && <div className="col-md-3 btn-primary" onClick={() => {startBreakfast()}}>START</div> }
                 { meals.breakfast.active && <div className="col-md-3 btn-danger" onClick={() => {stopBreakfast()}}>FINISH</div> }
                 { meals.breakfast.active && <div class="container col-md-2">
-                <Progress completed={percentage}/>
+                <Progress completed={breakfastPercentage}/>
                 </div> }
                 { meals.breakfast.active && <div className="col-md-1">{meals.breakfast.served.length}/{meals.breakfast.invited.length}</div> }
+                { !meals.breakfast.active && <div className="col-md-3">{meals.breakfast.served.length}/{meals.breakfast.invited.length}</div> }
               </div>
               <div className="row buttonRow">
                 <div className="col-md-6">Lunch</div>
                 { !meals.lunch.active && <div className="col-md-3 btn-primary" onClick={() => {startLunch()}}>START</div> }
                 { meals.lunch.active && <div className="col-md-3 btn-danger" onClick={() => {stopLunch()}}>FINISH</div> }
-                <div className="col-md-3">{meals.lunch.served.length}/{meals.lunch.invited.length}</div>
+                { meals.lunch.active && <div class="container col-md-2">
+                <Progress completed={lunchPercentage}/>
+                </div> }
+                { meals.lunch.active && <div className="col-md-1">{meals.lunch.served.length}/{meals.lunch.invited.length}</div> }
+                { !meals.lunch.active && <div className="col-md-3">{meals.lunch.served.length}/{meals.lunch.invited.length}</div> }
               </div>
               <div className="row buttonRow">
                 <div className="col-md-6">Dinner</div>
                 { !meals.dinner.active && <div className="col-md-3 btn-primary" onClick={() => {startDinner()}}>START</div> }
                 { meals.dinner.active && <div className="col-md-3 btn-danger" onClick={() => {stopDinner()}}>FINISH</div> }
-                <div className="col-md-3">{meals.dinner.served.length}/{meals.dinner.invited.length}</div>
+                { meals.dinner.active && <div class="container col-md-2">
+                <Progress completed={dinnerPercentage}/>
+                </div> }
+                { meals.dinner.active && <div className="col-md-1">{meals.dinner.served.length}/{meals.dinner.invited.length}</div> }
+                { !meals.dinner.active && <div className="col-md-3">{meals.dinner.served.length}/{meals.dinner.invited.length}</div> }
               </div>
           </div>
           <div className="topSide">
               {  meals.breakfast.active && <div className="row buttonRow">
-                {<div className="btn-primary" onClick={() => { updateMeal(meals.breakfast.name) }}>BRING MORE PEOPLE FOR BREAKFAST</div> }
+                <div className="btn-primary" onClick={() => { updateMeal(meals.breakfast.name) }}>BRING MORE PEOPLE FOR BREAKFAST</div>
               </div> }
               { meals.lunch.active &&  <div className="row buttonRow">
-                {<div className="btn-primary" onClick={() => { updateMeal(meals.lunch.name) }}>BRING MORE PEOPLE FOR LUNCH</div> }
+                <div className="btn-primary" onClick={() => { updateMeal(meals.lunch.name) }}>BRING MORE PEOPLE FOR LUNCH</div>
               </div> }
               { meals.dinner.active && <div className="row buttonRow">
-                {<div className="btn-primary" onClick={() => { updateMeal(meals.dinner.name) }}>BRING MORE PEOPLE FOR DINNER</div> }
+                <div className="btn-primary" onClick={() => { updateMeal(meals.dinner.name) }}>BRING MORE PEOPLE FOR DINNER</div>
               </div>}
             </div>
             <div className="row buttonRow">
-                {<div className="btn-primary" onClick={() => {activateChecker()}}>BRING UP CHECKER</div> }
+                <div className="btn-primary" onClick={() => {activateChecker()}}>BRING UP CHECKER</div>
             </div>
         </div>
       </div>
